63c3e7f00c1b38a68b469857c2bc29203d10a78d
praganya <praganyasingh03@gmail.com>
2026-08-09 12:35:09 +0530

    Delete model directory

--- model/resolution.py
+++
@@ -1,8 +0,0 @@
-from PIL import Image
-
-# Open the TIFF image
-img = Image.open("sample.tif")
-
-# Get image resolution in pixels
-width, height = img.size
-print(f"Image Resolution: {width} x {height} pixels")
